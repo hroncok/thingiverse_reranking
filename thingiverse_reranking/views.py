1c21b21a106f3d7c84f69143e5d611f164c83a58
Miro Hrončok <miro@hroncok.cz>
2014-11-29 16:55:42 +0100

    Allow to search from Thingiverse

--- thingiverse_reranking/views.py
+++ thingiverse_reranking/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.conf import settings
+import thingiverse
+
+
+KEY = settings.THINGIVERSE_KEY
+SECRET = settings.THINGIVERSE_SECRET
+TOKEN = settings.THINGIVERSE_TOKEN
 
 
 def index(request):
     '''The homepage'''
-    return render(request, 'index.html', {})
+    t = thingiverse.Thingiverse({'client_id': KEY, 'client_secret': SECRET, 'redirect_uri': ''})
+    t.connect(TOKEN)
+    term = request.GET.get('term')
+    if term:
+        results = t.keyword_search(term)
+    else:
+        term = ""
+        results = []
+    return render(request, 'index.html', {'term': term, 'results': results})
