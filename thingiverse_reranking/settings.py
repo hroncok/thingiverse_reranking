f23e59b8bd8211b46352ac8a7ada7fba7c32ce79
Miro Hrončok <miro@hroncok.cz>
2014-11-29 17:15:07 +0100

    Remove unnecessary content form settings.py

--- thingiverse_reranking/settings.py
+++ thingiverse_reranking/settings.py
@@ -30,8 +30,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -42,7 +40,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -55,13 +52,11 @@ WSGI_APPLICATION = 'thingiverse_reranking.wsgi.application'
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Europe/Prague'
 
-TIME_ZONE = 'UTC'
+USE_I18N = False
 
-USE_I18N = True
-
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
--- thingiverse_reranking/urls.py
+++ thingiverse_reranking/urls.py
@@ -5,5 +5,4 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'thingiverse_reranking.views.index'),
-    url(r'^admin/', include(admin.site.urls)),
 )
