c62633a6f672b7645ff534f9cdc9d1dc37fa510d
Miro Hrončok <miro@hroncok.cz>
2014-11-29 20:15:18 +0100

    Display more stuff about the things

--- thingiverse_reranking/views.py
+++ thingiverse_reranking/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.conf import settings
+from datetime import datetime
 import thingiverse
-
 
 KEY = settings.THINGIVERSE_KEY
 SECRET = settings.THINGIVERSE_SECRET
 TOKEN = settings.THINGIVERSE_TOKEN
-
+DATE_FORMAT = '%Y-%m-%d'
+DATE_LENGTH = 10
 
 def index(request):
     '''The homepage'''
@@ -15,6 +16,14 @@ def index(request):
     term = request.POST.get('term')
     if term:
         results = t.keyword_search(term)
+        for result in results:
+            # Get more info
+            result['detail'] = t.get_thing(int(result['id']))
+
+            # Calculate age
+            added = datetime.strptime(result['detail']['added'][:DATE_LENGTH], DATE_FORMAT)
+            delta = datetime.now() - added
+            result['detail']['age'] = delta.days
     else:
         term = ""
         results = []
