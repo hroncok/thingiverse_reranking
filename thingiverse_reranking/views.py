1b9258b636cf578a5d60a7f16b09546ab61c7722
Miro Hrončok <miro@hroncok.cz>
2014-11-29 22:54:11 +0100

    Calculate the duration of API calls + calculation

--- thingiverse_reranking/views.py
+++ thingiverse_reranking/views.py
@@ -26,9 +26,12 @@ def index(request):
     '''The homepage'''
     term = request.POST.get('term')
     if term:
+        times = []
+        times.append(datetime.now())
         t = thingiverse.Thingiverse({'client_id': KEY, 'client_secret': SECRET, 'redirect_uri': ''})
         t.connect(TOKEN)
         results = t.keyword_search(term)
+        times.append(datetime.now())
 
         maxs = {}
         normalized = {}
@@ -50,7 +53,9 @@ def index(request):
 
         for result in results:
             # Get more info
+            times.append(datetime.now())
             result['detail'] = t.get_thing(int(result['id']))
+            times.append(datetime.now())
 
             # Calculate age
             added = datetime.strptime(result['detail']['added'][:DATE_LENGTH], DATE_FORMAT)
@@ -80,7 +85,19 @@ def index(request):
                 result['relative'][feature] = normalize(result['absolute'][feature], maxs[feature], weights[feature])
                 result['penalty'] += result['relative'][feature]
         results = sorted(results, key=lambda k: k['penalty'])
+
+        times.append(datetime.now())
+        
+        # Calculate times
+        deltas = {}
+        deltas['total'] = times[4] - times[0]
+        deltas['api'] = (times[1] - times[0]) + (times[3] - times[2])
+        deltas['noapi'] = deltas['total'] - deltas['api']
     else:
         term = ""
         results = []
-    return render(request, 'index.html', {'term': term, 'results': results, 'post': request.POST})
+        deltas = {}
+    return render(request, 'index.html', {'term': term,
+                                          'results': results,
+                                          'post': request.POST,
+                                          'deltas': deltas})
